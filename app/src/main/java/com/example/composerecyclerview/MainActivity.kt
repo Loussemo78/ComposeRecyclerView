2fe0c8d201389464e9c95f35db007ecf356de7b5
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-06 20:26:57 +0200

    DisplayTvShows successfully added vith click and toast

--- app/src/main/java/com/example/composerecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/composerecyclerview/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,6 +38,10 @@ class MainActivity : ComponentActivity() {
             /*LazyColumnDemo2 {
                 Toast.makeText(this,it,Toast.LENGTH_LONG).show()
             }*/
+            
+            DisplayTvShows{
+                Toast.makeText(this,it.name,Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
@@ -87,6 +93,24 @@ fun LazyColumnDemo2(selectedItem:(String)->(Unit)){
     }
 }
 //Displaying TvShow List
+@Composable
+fun DisplayTvShows(selectedItem: (TvShow) -> Unit){
+
+    val tvShows = remember{ TvShowList.tvShows }
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp))
+    {
+        items(
+            items = tvShows,
+            itemContent = {
+                TvShowListItem(tvShow = it, selectedItem = selectedItem)
+            }
+        )
+    }
+}
+
+
+
 
 @Composable
 fun TvShowListItem(tvShow: TvShow,selectedItem: (TvShow) -> Unit){

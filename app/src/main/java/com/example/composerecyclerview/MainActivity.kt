4b9b62c729de30e55dd67d25615adea76094f95a
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-06 20:31:58 +0200

    clean code and created package compose

--- app/src/main/java/com/example/composerecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/composerecyclerview/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composerecyclerview.compose.TvShowListItem
 import com.example.composerecyclerview.data.TvShowList
 import com.example.composerecyclerview.model.TvShow
 import com.example.composerecyclerview.ui.theme.ComposeRecyclerViewTheme
@@ -112,56 +113,4 @@ fun DisplayTvShows(selectedItem: (TvShow) -> Unit){
 
 
 
-@Composable
-fun TvShowListItem(tvShow: TvShow,selectedItem: (TvShow) -> Unit){
-    Card(
-        modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(),
-        elevation = 10.dp,
-        shape = RoundedCornerShape(corner = CornerSize(10.dp))
-    ) {
-       Row(
-           modifier = Modifier
-               .padding(5.dp)
-               .fillMaxWidth()
-               .clickable {
-                   selectedItem(tvShow)
-               },
-           verticalAlignment = Alignment.CenterVertically
-       ) {
-          TvShowImage(tvShow = tvShow)
-           Column {
-               Text(text = tvShow.name, style = MaterialTheme.typography.h5)
-               Spacer(modifier = Modifier.height(4.dp))
-               Text(text = tvShow.overview,
-               style = MaterialTheme.typography.body1,
-               maxLines = 3,
-               overflow = TextOverflow.Ellipsis)
-               Spacer(modifier = Modifier.height(8.dp))
-               Row(
-                   modifier = Modifier.fillMaxWidth(),
-                   horizontalArrangement = Arrangement.SpaceBetween
-               ) {
-                   Text(text = tvShow.year.toString(), style = MaterialTheme.typography.h5)
-                   Text(text = tvShow.rating.toString(), style = MaterialTheme.typography.h5)
-               }
 
-           }
-       }
-    }
-}
-
-@Composable
-private fun TvShowImage(tvShow: TvShow){
-    Image(
-        painter = painterResource(id = tvShow.imageId) ,
-    contentDescription = null,
-    contentScale = ContentScale.Crop,
-    modifier = Modifier
-        .padding(4.dp)
-        .height(140.dp)
-        .width(100.dp)
-        .clip(RoundedCornerShape(corner = CornerSize(10.dp)))
-    )
-}
